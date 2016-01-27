6037be2307a4583ec06dabb9ab0ef76ceb1cc8d0
vanilija <digitalni2k@gmail.com>
2016-01-27 22:25:31 +0100

    Added couple lines of code

---
+++ Drinago/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Drinago.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GeneralInfo()
+        {
+            return View();
+        }
+
+        public ActionResult Transportation()
+        {
+            return View();
+        }
+
+        public ActionResult WhereTo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Drinago/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Drinago.MvcApplication" Language="C#" %>
