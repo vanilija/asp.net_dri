25c56922ee048ddf8247b398b6eb920cf3440a09
vanilija <digitalni2k@gmail.com>
2016-02-02 18:52:24 +0100

    Changed navbar, included more content, fixed sliders.

--- Drinago/Controllers/HomeController.cs
+++ Drinago/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Drinago.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Basic Info";
+            ViewBag.Message = "Hello";
             ViewBag.Message2 = "Topographical position";
             ViewBag.Message3 = "Area of Gorazde";
             ViewBag.Message4 = "Ethnicity and Religion";
@@ -22,7 +22,8 @@ namespace Drinago.Controllers
         {
             ViewBag.Message = "Editors Notice";
             ViewBag.Message2 = "Our Vision";
-            ViewBag.Message3 = "Team behind the project";
+            ViewBag.Message3 = "Notice about registration";
+            ViewBag.Message4 = "Team behind the project";
             return View();
         }
 
@@ -35,6 +36,14 @@ namespace Drinago.Controllers
         public ActionResult Info()
         {
             ViewBag.Message = "Additional Information";
+            ViewBag.Message1 = "Basic Info";
+            ViewBag.Message2 = "Topographical position";
+            ViewBag.Message3 = "Area of Gorazde";
+            ViewBag.Message4 = "Ethnicity and Religion";
+            ViewBag.Message5 = "Climate, electricity, water supply, currency";
+            ViewBag.Message6 = "Population";
+            ViewBag.Message7 = "Time zone";
+            ViewBag.Message8 = "Holidays";
             return View();
         }
 
@@ -56,6 +65,71 @@ namespace Drinago.Controllers
             ViewBag.Message3 = "Monza 2";
             ViewBag.Message4 = "Zute Dunje";
             ViewBag.Message5 = "Ciro";
+            ViewBag.Message6 = "";
+
+            return View();
+        }
+
+        public ActionResult WhatToDo()
+        {
+            ViewBag.Message = "Things to do";
+            ViewBag.Message1 = "";
+            ViewBag.Message2 = "";
+            ViewBag.Message3 = "";
+            ViewBag.Message4 = "";
+            ViewBag.Message5 = "";
+            ViewBag.Message6 = "";
+
+            return View();
+        }
+
+        public ActionResult WhereToStay()
+        {
+            ViewBag.Message = "Where to Stay";
+            ViewBag.Message1 = "Hotel Behar";
+            ViewBag.Message2 = "Casablanca";
+            ViewBag.Message3 = "";
+            ViewBag.Message4 = "";
+            ViewBag.Message5 = "";
+            ViewBag.Message6 = "";
+
+            return View();
+        }
+
+        public ActionResult WhereToShop()
+        {
+            ViewBag.Message = "Where to Shop";
+            ViewBag.Message1 = "";
+            ViewBag.Message2 = "";
+            ViewBag.Message3 = "";
+            ViewBag.Message4 = "";
+            ViewBag.Message5 = "";
+            ViewBag.Message6 = "";
+
+            return View();
+        }
+
+        public ActionResult WhereToEat()
+        {
+            ViewBag.Message = "Food and Drink";
+            ViewBag.Message1 = "Hotel Behar";
+            ViewBag.Message2 = "Casablanca";
+            ViewBag.Message3 = "Monza 2";
+            ViewBag.Message4 = "Zute Dunje";
+            ViewBag.Message5 = "Ciro";
+            ViewBag.Message6 = "";
+
+            return View();
+        }
+
+        public ActionResult WhatToSee()
+        {
+            ViewBag.Message = "What to See";
+            ViewBag.Message1 = "";
+            ViewBag.Message2 = "";
+            ViewBag.Message3 = "";
+            ViewBag.Message4 = "";
+            ViewBag.Message5 = "";
             ViewBag.Message6 = "";
 
             return View();
